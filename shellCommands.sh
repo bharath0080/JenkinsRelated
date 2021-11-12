0938358e54be77f830673b83f8f45121df045b9c
bharath0080 <bharathforany@gmail.com>
2021-11-12 23:56:36 +0530

    Update and rename shellCommands.md to shellCommands.sh

--- shellCommands.sh
+++ shellCommands.sh
@@ -1,11 +1,12 @@
-## Get only numericals words from sentence 
-
-```
+############################################
+## Get only numericals words from sentence
+############################################
 #Different examples
 #testing="Release-Prod-1.0.0-10.9.1"
 #testing="Release-Prod-1.0.0"
 testing="Release-Prod-1.0.0-12.5.0"
-
 values=`echo $testing | awk -F"-" '{ for (i=1; i<=NF; i++) if( $i~/^[0-9]/) printf "%s-", $i}' | sed 's/-$//'`
 echo $values
-```
+
+
+############################################
